4dc4d157b8c420484be796c824e860bda399baed
Philimuhire <philimuhire@gmail.com>
2024-06-19 12:09:48 +0200

    initialized homepage

---
+++ src/Routes/Homepage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Components/Homepage_header';
+import Footer from '../Components/Homepage_footer';
+
+
+const Homepage: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default Homepage;
