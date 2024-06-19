f9ddcbdb2aa4340cfc783fed85f57f1d384248a0
Philimuhire <philimuhire@gmail.com>
2024-06-19 12:11:47 +0200

    added homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.css';
+import Homepage from './Routes/Homepage';
 import image from "./asset/images/home-page-dealflow-4.jpg"
 import { LazyLoadImage } from "react-lazy-load-image-component"
 
 const App: React.FC = () => (
-  <div className="">
-    
-  </div>
+  <Router>
+    <div className="">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </div>
+  </Router>
 );
 
 export default App;
