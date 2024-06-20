896090ca2f375cc6969d568c83bd3987bf966651
Philimuhire <philimuhire@gmail.com>
2024-06-20 08:50:51 +0200

    added ContactSection component

--- src/Routes/Homepage.tsx
+++ src/Routes/Homepage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Components/Homepage_header';
 import Footer from '../Components/Homepage_footer';
+import ContactSection from '../Components/ContactSection';
 
 
 const Homepage: React.FC = () => {
   return (
     <div>
       <Header />
+      <ContactSection />
       <Footer />
     </div>
   );
