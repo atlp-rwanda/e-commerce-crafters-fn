99b57c43766eaeeddc0a6b6a318bab81acb2dbfe
sevelinCA <ngabosevelin@gmail.com>
2024-06-21 20:07:31 +0200

    Solve Merge Conflicts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-<<<<<<< HEAD
       colors: {
         primary: '#013362',
         secondary: '#C9974C',
@@ -30,32 +29,6 @@ module.exports = {
         '3xl': '1440px',
         '4xl': '1600px',
         '5xl': '1920px',
-=======
-      colors:{
-        primary: "#013362",
-        secondary: "#C9974C",
-        white: "#fff",
-        black: "#000",
-        gray:{
-          100: "#EEEDED",
-          200: "#CACACA"
-        },
-        border: "#666666"
-      },
-      fontFamily:{
-          outfit: ['Outfit', 'sans-serif'],
-          poppins: ['Poppins', 'sans-serif'],
-      },
-      screens: {
-        'sm': '360px', 
-        'md': '540px', 
-        'lg': '768px',
-        'xl': '1024px', 
-        '2xl': '1280px',
-        '3xl': '1440px',
-        '4xl': '1600px', 
-        '5xl': '1920px', 
->>>>>>> 1a6a9586f92c62ed2248a9d48cb2ea374325e3a8
       },
     },
   },
