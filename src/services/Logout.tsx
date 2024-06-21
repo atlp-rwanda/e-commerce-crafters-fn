6827f49d06ae2172f2c477c9c08561c3ef9b90d0
sevelinCA <ngabosevelin@gmail.com>
2024-06-21 19:42:06 +0200

    Adding Google Auth

--- src/services/Logout.tsx
+++ src/services/Logout.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import useSignOut from 'react-auth-kit/hooks/useSignOut';
 
@@ -11,17 +10,3 @@ const Logout = () => {
 };
 
 export default Logout;
-=======
-import React from 'react'
-import useSignOut from 'react-auth-kit/hooks/useSignOut'
-
-const Logout = () => {
-    const signOut = useSignOut()
-   return  ()=>{
-        signOut()
-        window.location.href="login"
-    }
-}
-
-export default Logout
->>>>>>> 1a6a9586f92c62ed2248a9d48cb2ea374325e3a8
