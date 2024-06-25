eeb8112cfafbf25b579c908787f7801bc4f0592a
Philimuhire <philimuhire@gmail.com>
2024-06-25 07:03:59 +0200

    imported productsImages Reducer

--- src/Redux/store.tsx
+++ src/Redux/store.tsx
@@ -1,11 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, Action } from '@reduxjs/toolkit';
 import AuthSlice from './features/AuthSlice';
+import { ThunkAction } from 'redux-thunk';
+import productsImagesReducer from './features/ProductsImagesSlice';
 
 export const store = configureStore({
     reducer: {
       auth: AuthSlice,
+      productsImages: productsImagesReducer
     }
   });
   
   export type RootState = ReturnType<typeof store.getState>;
   export type AppDispatch = typeof store.dispatch;
+  export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
