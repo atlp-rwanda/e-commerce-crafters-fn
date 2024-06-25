e1660527a19946d3040794c1a25790b9de5a68d8
Philimuhire <philimuhire@gmail.com>
2024-06-25 07:51:28 +0200

    solved conflicts

--- src/Redux/store.tsx
+++ src/Redux/store.tsx
@@ -1,13 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, Action } from '@reduxjs/toolkit';
 import AuthSlice from './features/AuthSlice';
-import { apiSlice } from './features/ApiSlice';
+import { ThunkAction } from 'redux-thunk';
+import productsImagesReducer from './features/ProductsImagesSlice';
 
 export const store = configureStore({
-  reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
-  },
-  middleware: (getDefaultMiddelware) => getDefaultMiddelware().concat(apiSlice.middleware),
-});
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+    reducer: {
+      productsImages: productsImagesReducer
+    }
+  });
+  
+  export type RootState = ReturnType<typeof store.getState>;
+  export type AppDispatch = typeof store.dispatch;
+  export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
