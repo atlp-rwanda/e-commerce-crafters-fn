7154f934aba87ad26e7fb53ea7900e0938c2b81f
ismaelmurekezi <ismaelmurekezi1@gmail.com>
2024-06-26 15:18:47 +0200

    fix bug

--- src/pages/Check.tsx
+++ src/pages/Check.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const Check = () => {
   return (
@@ -6,9 +6,12 @@ const Check = () => {
       <h1 className="text-gray-800">
         If you want to part of the program click button below
       </h1>
-      <button className='  py-2 px-7 ml-10 mt-5 bg-purple-700 text-white rounded-md hover:bg-purple-500'>Confirm</button>
+      <button className="  py-2 px-7 ml-10 mt-5 bg-purple-700 text-white rounded-md hover:bg-purple-500">
+        Confirm
+      </button>
+      <h1>hello there</h1>
     </div>
   );
-}
+};
 
-export default Check
+export default Check;
