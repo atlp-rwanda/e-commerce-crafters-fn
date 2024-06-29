8714478fae1d1003ad895aedf074e6187b2fc69f
Philimuhire <philimuhire@gmail.com>
2024-06-29 22:22:26 +0200

    added integration on contact

--- src/Redux/store.tsx
+++ src/Redux/store.tsx
@@ -2,14 +2,16 @@ import { configureStore, Action } from '@reduxjs/toolkit';
 import { ThunkAction } from 'redux-thunk';
 import productsImagesReducer from './features/ProductsImagesSlice';
 import popularProductsReducer from './features/PopularProductsSlice';
+import contactReducer from './features/contactSlice';
 
 export const store = configureStore({
     reducer: {
       productsImages: productsImagesReducer,
-      popularProducts: popularProductsReducer
+      popularProducts: popularProductsReducer,
+      contact: contactReducer
     }
   });
   
   export type RootState = ReturnType<typeof store.getState>;
   export type AppDispatch = typeof store.dispatch;
-  export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
+  export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
