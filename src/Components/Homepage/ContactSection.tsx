556767f7e1bf139037e3b3eacff232093a96bd53
Philimuhire <philimuhire@gmail.com>
2024-07-10 15:49:20 +0200

    solved conflicts

--- src/Components/Homepage/ContactSection.tsx
+++ src/Components/Homepage/ContactSection.tsx
@@ -33,7 +33,7 @@ const ContactSection: React.FC<Props> = ({ status, error, sendMessage }) => {
   };
   const { t } = useTranslation();
   return (
-    <section className="px-8 md:px-64 py-16 bg-gray-100 flex flex-col md:flex-row justify-center font-outfit md:text-xl md:space-x-40 h-screen mt-24">
+    <section className="px-8 md:px-64 py-16 bg-gray-100 flex flex-col md:flex-row justify-center font-outfit md:text-xl md:space-x-40 sm:h-auto md:h-screen mt-24 sm:mt-8">
       <div className="w-full md:w-1/2 mb-8 md:mb-0">
         <h2 className="text-primary text-3xl md:text-4xl font-bold mb-4">
           {t("Get In Touch With Us")}
